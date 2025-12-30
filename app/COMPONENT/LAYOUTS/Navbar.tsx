f653464e755033aed3072c2f2578053e44bd5a8e
Dip908 <dipchondo007@gmail.com>
2025-12-30 10:02:00 +0600

    design the navbar

--- app/COMPONENT/LAYOUTS/Navbar.tsx
+++ app/COMPONENT/LAYOUTS/Navbar.tsx
@@ -7,24 +7,24 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className='grid grid-cols-6 border'>
+        <div className='grid grid-cols-6 border-2 border-(--secondary-color)'>
             {/* left */}
             <div className='col-span-1'></div>
 
             {/* middle */}
-            <div className='col-span-4 grid grid-cols-6 border-x pl-1.5'>
+            <div className='col-span-4 grid grid-cols-6 border-x-2 border-(--secondary-color) pl-1.5'>
                 {/* logo */}
-                <div className='col-span-2 border-r py-2 flex gap-1 items-center'>
+                <div className='col-span-2 border-r-2 border-(--secondary-color) py-2 flex gap-1 items-center'>
                     <ThemeChanger/>
-                    <Link href={'/'} className='cursor-pointer font-bold text-2xl'>NARRATEIT</Link>
+                    <Link href={'/'} className='cursor-pointer font-bold text-2xl text-(--accent-color)'>NARRATEIT</Link>
                 </div>
 
                 {/* nav items */}
-                <div className='col-span-4 flex justify-between items-center px-1.5'>
-                    <h1 className='cursor-pointer font-bold text-xl'>HOME</h1>
-                    <h1 className='cursor-pointer font-bold text-xl'>BLOGS</h1>
-                    <h1 className='cursor-pointer font-bold text-xl'>LOGIN</h1>
-                    <h1 className='cursor-pointer font-bold text-xl'>REGISTER</h1>
+                <div className='col-span-4 flex justify-between items-center'>
+                    <h1 className='cursor-pointer font-bold text-xl border-r-2 border-(--secondary-color) h-full items-center flex flex-1 px-1.5 text-(--accent-color)'>HOME</h1>
+                    <h1 className='cursor-pointer font-bold text-xl border-r-2 border-(--secondary-color) h-full items-center flex flex-1 px-1.5 text-(--accent-color)'>BLOGS</h1>
+                    <h1 className='cursor-pointer font-bold text-xl border-r-2 border-(--secondary-color) h-full items-center flex flex-1 px-1.5 text-(--accent-color)'>LOGIN</h1>
+                    <h1 className='cursor-pointer font-bold text-xl h-full items-center flex flex-1 px-1.5 text-(--accent-color)'>REGISTER</h1>
                 </div>
             </div>
 

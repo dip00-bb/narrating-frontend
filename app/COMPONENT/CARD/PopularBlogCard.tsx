c0e9fc0fb0fd84f6a2797fd2a635a647ed80b5ad
Dip908 <dipchondo007@gmail.com>
2025-12-31 12:21:38 +0600

    create popular blog card

---
+++ app/COMPONENT/CARD/PopularBlogCard.tsx
@@ -0,0 +1,33 @@
+import { smoochSans } from '@/app/FONTS/fonts';
+import { Calendar } from 'lucide-react';
+import Image from 'next/image';
+import React from 'react';
+
+const PopularBlogCard = () => {
+    return (
+        <div className='flex items-center justify-between border-b-2 border-(--secondary-color) pb-3'>
+            <div>
+                <p className={`text-xl md:text-2xl ${smoochSans.className}`}>Time slowly fades where moss grows</p>
+
+                <div className='flex items-center gap-0.5'>
+                    <Calendar size={15} />
+                    <p className={`${smoochSans.className}`}>11/1/2024</p>
+                </div>
+            </div>
+
+            <div>
+                <div className="w-full relative">
+                    <Image
+                        src="/heroforeground.jpg"
+                        alt="profile"
+                        width={150}
+                        height={150}
+                        className='rounded'
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PopularBlogCard;
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Navbar from './COMPONENT/LAYOUTS/Navbar';
 import BannerHeader from './COMPONENT/BANNER/BannerHeader';
 import BannerFooter from './COMPONENT/BANNER/BannerFooter';
 import BlogCard from './COMPONENT/CARD/BlogCard';
+import PopularBlogCard from './COMPONENT/CARD/PopularBlogCard';
+import { smoochSans } from './FONTS/fonts';
 
 
 const Home = () => {
@@ -30,19 +32,42 @@ const Home = () => {
 
 
           {/* Top Blogs */}
-          <div className='grid grid-cols-3 '>
+          <div className='grid grid-cols-1 md:grid-cols-3 md:gap-10'>
             {/* card */}
-            <div className='col-span-2 space-y-3.5'>
-              <BlogCard />
-              <BlogCard />
-              <BlogCard />
-              <BlogCard />
-              <BlogCard />
+            <div className='col-span-2'>
+              <div className='mb-4'>
+                <p className={`mb-3.5 secondery-fontsize font-bold ${smoochSans.className}`}>RECENT BLOG</p>
+                <hr className='border-dotted' />
+                <hr className='border-dotted' />
+                <hr className='border-dotted' />
+                <hr className='border-dotted' />
+                <hr className='border-dotted' />
+              </div>
+              <div className='space-y-3.5'>
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+                <BlogCard />
+              </div>
             </div>
 
             {/* popular blog */}
             <div>
-          
+              <div className='mb-4'>
+                <p className={`mb-3.5 secondery-fontsize font-bold ${smoochSans.className}`}>POPULAR BLOGS</p>
+                <hr className='border-dotted' />
+                <hr className='border-dotted' />
+                <hr className='border-dotted' />
+                <hr className='border-dotted' />
+                <hr className='border-dotted' />
+              </div>
+              <div className='space-y-3.5'>
+                <PopularBlogCard />
+                <PopularBlogCard />
+                <PopularBlogCard />
+                <PopularBlogCard />
+              </div>
             </div>
           </div>
 

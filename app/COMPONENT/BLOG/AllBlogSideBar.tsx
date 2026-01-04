019e5c2b4fdc8f89e27f65c4eee6a31eaf969375
Dip908 <dipchondo007@gmail.com>
2026-01-04 12:42:08 +0600

    create sidebar responsive for all over the devices

--- app/COMPONENT/BLOG/AllBlogSideBar.tsx
+++ app/COMPONENT/BLOG/AllBlogSideBar.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import SelectFilter from './SelectFilter';
 import { smoochSans } from '@/app/FONTS/fonts';
+import { Button } from '@/components/ui/button';
+import { X } from 'lucide-react';
 
-const AllBlogSideBar = () => {
+interface IsidebarProps{
+    setPanelOpen:(arg:boolean)=>void
+}
+
+const AllBlogSideBar = ({setPanelOpen}:IsidebarProps) => {
     return (
-        <div className='space-y-4'>
-            <div className='space-y-2.5'>
+        <div className='space-y-4 py-4 md:py-0'>
+            <div className='space-y-2.5 hidden md:block '>
                 <p className={`${smoochSans.className} text-xl md:text-2xl`}> Search By Author or Title</p>
-                <div className='border flex items-center gap-2 '>
+                <div className='border flex items-center gap-2 w-2xs '>
                     <input placeholder='SEARCH BY AUTHOR OR TOPIC' className='border-0 outline-0 py-3 w-full px-2' />
                 </div>
             </div>
 
-            <div className='space-y-2.5'>
-                <p className={`${smoochSans.className} text-xl md:text-2xl`}>POPULAR TAGS</p>
-                <div className='flex flex-wrap gap-2'>
-                    <p className={`${smoochSans.className} text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>SPORTS</p>
-                    <p className={`${smoochSans.className} text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>GAMING</p>
-                    <p className={`${smoochSans.className} text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>TECHNOLOGY</p>
-                    <p className={`${smoochSans.className} text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>PROGRAMMING</p>
-                    <p className={`${smoochSans.className} text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>SCIFI</p>
-                    <p className={`${smoochSans.className} text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>MOVIES</p>
+            <div className='space-y-2.5 w-fit'>
+                <div className='flex justify-between'><p className={`${smoochSans.className} text-xl md:text-2xl`}>POPULAR TAGS</p> <span onClick={()=>setPanelOpen(false)} className='md:hidden'><X/></span> </div>
+                <div className='grid grid-cols-2 md:grid-cols-3 gap-2'>
+                    <p className={`${smoochSans.className} text-xl md:text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>SPORTS</p>
+                    <p className={`${smoochSans.className} text-xl md:text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>GAMING</p>
+                    <p className={`${smoochSans.className} text-xl md:text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>TECHNOLOGY</p>
+                    <p className={`${smoochSans.className} text-xl md:text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>PROGRAMMING</p>
+                    <p className={`${smoochSans.className} text-xl md:text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>SCIFI</p>
+                    <p className={`${smoochSans.className} text-xl md:text-2xl font-semibold border py-1 px-2 hover:bg-(--secondary-color) hover:text-white cursor-pointer`}>MOVIES</p>
                 </div>
             </div>
 
@@ -28,6 +34,11 @@ const AllBlogSideBar = () => {
             <div className='space-y-2.5'>
                 <p className={`${smoochSans.className} text-xl md:text-2xl`}>FILTER BY</p>
                 <div><SelectFilter /></div>
+            </div>
+
+            <div className='flex justify-between'>
+                <Button>Reset</Button>
+                <Button onClick={()=>setPanelOpen(false)}>Search</Button>
             </div>
         </div>
     );
--- app/COMPONENT/BLOG/AllBlogs.tsx
+++ app/COMPONENT/BLOG/AllBlogs.tsx
@@ -1,20 +1,40 @@
-import React from 'react';
-import AllBlogCards from '../MAPPEDCARD/AllBlogCards';
-import AllBlogSideBar from './AllBlogSideBar';
-
+"use client"
+import AllBlogCards from "../MAPPEDCARD/AllBlogCards";
+import AllBlogSideBar from "./AllBlogSideBar";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Filter, } from "lucide-react";
 
 const AllBlogs = () => {
+
+
+    const [panelOpen, setPanelOpen] = useState(false)
+    const handleSearch=(e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        console.log("Hello")
+    }
+
     return (
-        <div className='grid grid-cols-1 md:grid-cols-6 px-2 '>
+        <div className='grid grid-cols-1 md:grid-cols-6'>
+            <div className="md:hidden flex gap-2.5 justify-between  p-2 overflow-hidden">
+
+                <form onSubmit={handleSearch} className='border flex items-center gap-2 w-2xs'>
+                    <input placeholder='SEARCH BY AUTHOR OR TOPIC' className='border-0 outline-0 flex-1 px-1' />
+                </form>
+
+                <Button onClick={()=>setPanelOpen(true)} className="border bg-transparent text-(--accent-color) cursor-pointer"><Filter className="text-(--accent-color)" /> Filter</Button>
+
+            </div>
+
             <div className='col-span-1'></div>
-            <div className='col-span-4 grid grid-cols-1 md:grid-cols-6 '>
-                <div className='col-span-4'>
-                    <AllBlogCards/>
+            <div className='col-span-4 grid grid-cols-2 md:grid-cols-6 p-2'>
+                <div className="col-span-4">
+                    <AllBlogCards />
                 </div>
 
-                <div className='col-span-2 hidden md:block'>
-                    <div className='px-4 space-y-10 sticky top-0'>
-                        <AllBlogSideBar/>
+                <div className={`col-span-2 `} >
+                    <div className={` fixed top-0 bg-gray-800 h-screen ${panelOpen ? 'right-0' :'-right-96'} md:sticky md:bg-transparent md:top-4`}>
+                        <div className="px-2 "><AllBlogSideBar setPanelOpen={setPanelOpen}/></div>
                     </div>
                 </div>
             </div>
@@ -24,3 +44,5 @@ const AllBlogs = () => {
 };
 
 export default AllBlogs;
+
+
